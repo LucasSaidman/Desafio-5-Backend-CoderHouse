10dcacc9d15d25d4be6c95b70edcb7aa6ce3d268
Lucas_Saidman <saidmanlucas.p@gmail.com>
2024-05-04 11:39:52 -0300

    Desafio 5

---
+++ src/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const PUERTO = 8080;
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const socket = require("socket.io");
+require("./database.js");
+
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
+const sessionRouter = require("./routes/session.router.js");
+const userRouter = require("./routes/user.router.js");
+
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("./src/public"));
+
+
+//Session
+app.use(session({
+    secret:"secretCoder",
+    resave: true, 
+    saveUninitialized:true,   
+}))
+
+
+//Configuramos Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+
+//Rutas
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/api/users", userRouter);
+app.use("/api/sessions", sessionRouter);
+app.use("/", viewsRouter);
+
+
+//Listen
+const httpServer = app.listen(PUERTO, () => {
+
+    console.log(`Escuchando en el puerto http://localhost:${PUERTO}`);
+
+});
+
+
+
+//Desafio del chat en el ecommerce: 
+const MessageModel = require("./models/message.model.js");
+const io = new socket.Server(httpServer);
+
+io.on("connection",  (socket) => {
+    console.log("Nuevo usuario conectado");
+
+    socket.on("message", async data => {
+
+        //Guardo el mensaje en MongoDB: 
+        await MessageModel.create(data);
+
+        //Obtengo los mensajes de MongoDB y se los paso al cliente: 
+        const messages = await MessageModel.find();
+        console.log(messages);
+        io.sockets.emit("message", messages);
+     
+    })
+})
